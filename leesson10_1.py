1ca39e55759daec2598bbba62a4b6ace12dd88f3
MariaHai <Maria12345>
2022-06-04 10:37:29 +0200

    responce text

--- leesson10_1.py
+++ leesson10_1.py
@@ -1,4 +1,11 @@
-import  urllib.request
-opener = urllib.request.build_opener()
-response = opener.open("https://httpbin.org/get")
-print(response.read())
+# import  urllib.request
+# opener = urllib.request.build_opener()
+# response = opener.open("https://httpbin.org/get")
+# print(response.read())
+
+import  requests
+response = requests.get("https://httpbin.org/get")
+print(response.content)
+print(f"Datatype - {type(response.content)}")
+print(response.text)
+print(f"Datatype - {type(response.text)}")
